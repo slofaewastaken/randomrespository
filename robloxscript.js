006ad4d7cd8875b5d074102d9f1a6238abb5847c
slofae <felixjd78822@gmail.com>
2025-10-20 23:04:11 -0400

    fixed stupid me toggle button collect boxes
    mobile supported gui

--- robloxscript.js
+++ robloxscript.js
@@ -1,11 +1,15 @@
-local UserInputService = game:GetService("UserInputService")
+local p = game.Players.LocalPlayer
+local h = p.Character.HumanoidRootPart
+local s, pos = false, h.CFrame
 
-    UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
-        if input.UserInputType == Enum.UserInputType.Keyboard then
-            if input.KeyCode == Enum.KeyCode.E then
-        end
-    end 
-end)
+local g = Instance.new("ScreenGui", p.PlayerGui)
+local b = Instance.new("TextButton", g)
+b.Size,b.Position,b.TextScaled,b.BackgroundColor3 = UDim2.new(0,80,0,25), UDim2.new(.5,-40,.9,0), true, Color3.new()
+b.Text,b.TextColor3 = "OFF", Color3.new(1,0,0)
+b.Active = true b.Draggable = true
+Instance.new("UICorner",b).CornerRadius = UDim.new(0,10)
+
+b.MouseButton1Click:Connect(function()
     s = not s
     b.Text, b.TextColor3 = s and "ON" or "OFF", s and Color3.new(0,1,0) or Color3.new(1,0,0)
     if s then pos = h.CFrame else h.CFrame = pos end
